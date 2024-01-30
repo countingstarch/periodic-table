0fd14150099f06861ad12ed2ce3f78b0e46fbb86
countingstarch <121007653+countingstarch@users.noreply.github.com>
2024-01-30 12:21:13 +0000

    Update table.js

--- table.js
+++ table.js
@@ -1,19 +1,37 @@
-const elementElements = document.querySelectorAll('.element');
+const meta = fetch('./element-data.json')
+    .then((res) => res.json())
+    .then((data) => {
+        main(data);
+    });
 
-const colour = document.querySelectorAll('.colour');
-const obj = {
-    'c': ['property', 'block', 'phase', 'custom']
-}
+const main = (data) => {
+    const elementElements = document.querySelectorAll('.element');
 
-const classify = (pre, fix) => {
-    for (i of elementElements) {
-        for (n of obj[pre]) {
-            i.classList.remove(`${pre}-${n}`)
-        }; i.classList.add(`${pre}-${fix}`);
+    const colour = document.querySelectorAll('.colour');
+
+    const obj = {
+        'colour': ['property', 'block', 'phase', 'custom']
     };
-};
 
-colour[0].addEventListener('change', () => classify('c', 'property'));
-colour[1].addEventListener('change', () => classify('c', 'block'));
-colour[2].addEventListener('change', () => classify('c', 'phase'));
-colour[3].addEventListener('change', () => classify('c', 'custom'));
+    const classify = (pre, fix) => {
+        for (i of elementElements) {
+            for (n of obj[pre]) {
+                i.classList.remove(`${pre}-${n}`);
+            }; i.classList.add(`${pre}-${fix}`);
+        };
+    }; for (i in obj) {
+        for (j of obj[i].filter((val) => val != 'custom')) {
+            for (e of elementElements) {
+                e.classList.add(`${j}-${data[e.id][j]}`);
+            };
+        };
+    };
+
+    colour[0].addEventListener('change', () => classify('colour', 'property'));
+    colour[1].addEventListener('change', () => classify('colour', 'block'));
+    colour[2].addEventListener('change', () => classify('colour', 'phase'));
+    colour[3].addEventListener('change', () => classify('colour', 'custom'));
+
+    // default colour scheme
+    colour.forEach(classify('colour', 'property'));
+};
